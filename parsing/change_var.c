e59d1e96fef0348be5dcb4cd588d0a276362ca24
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-07-30 17:54:33 +0100

    work for nominette

--- parsing/change_var.c
+++ parsing/change_var.c
@@ -1,303 +1,116 @@
 
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   change_var.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zbakkas <zouhirbakkas@gmail.com>           #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-07-28 15:45:21 by zbakkas           #+#    #+#             */
+/*   Updated: 2024-07-28 15:45:21 by zbakkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "min.h"
 
 
-static char  * search_env(t_envp *env, char *str)
+static int	check_erroe_var(char *var, int j, int l, char *str)
 {
-	char	*s1;
-	int		len_str;
+	int	ll;
+	int	k;
 
-	len_str = ft_strlen(str);
-	while (env)
+	ll = -1;
+	k = 0;
+	while (var && var[++ll] && !k)
+		if (var[ll] == ' ')
+			k = 1;
+	ll = j;
+	if (l != 2 && str[j])
 	{
-		if (!ft_strncmp(env->env, str, len_str) && *(env->env + len_str) == '=')
-			return (env->env + len_str + 1);
-		env = env->next;
+		while (ll >= 0)
+		{
+			ll--;
+			if ((str[ll] == '>' || str[ll] == '<' ))
+			{
+				if ((!var || k))
+					return (1);
+			}
+			if (str[ll] != ' ')
+				break ;
+		}
 	}
-	return (NULL);
+	return (0);
 }
 
-static char *get_valu_of_var(char *str,int *j)
+//$? exit_status
+static void	change_var_one(int *x, char *re, int *i)
 {
-    char *re;
-    int x =0;
-    int is_v=0;
-    int i=0;
-    while (str[x])
-    {
+	char	*str;
+	int		a;
 
-        if(is_v)
-        {
-            if(str[x]==' ' ||  str[x]=='\''|| str[x]=='"')
-                break;
-            i++;
-            (*j)++;
-        }
-        if(str[x]=='$')
-            is_v =1;
-        x++;
-    }
-    re=malloc(i+1);
-    // printf("%d\n",i);
-    i =0;
-    x =0;
-    is_v =0;
-    while (str[x])
-    {
-
-        if(is_v)
-        {
-            if(str[x]==' ' ||  str[x]=='\''|| str[x]=='"')
-                break;
-            re[i++]=str[x];
-        }
-        if(str[x]=='$')
-            is_v =1;
-        x++;
-    }
-    re[i]='\0';
-    // (*j)++;
-    return re;
+	str = ft_itoa(exit_status);
+	a = 0;
+	while (str[a])
+	{
+		re[(*i)++] = str[a++];
+	}
+	free(str);
+	(*x)++;
 }
 
-
-char *change_var(char * str,int *err)
+static void change_var_tow(t_args_var *args,char *str,int *err,char **envp)
 {
-    char *re;
-    int x =0;
-    int q=0;
-    int qq=0;
-    char * var= NULL;
-    int i=0;
-    int  j =0;
+	int		j;
+	char	*ss;
+	char	*var;
 
-    while (str[x])
-    {
-        if (str[x] == '\'' ||  str[x] == '"') 
-        {
-            if(qq ==0)
-            {
-                q=str[x];
-                qq++;
-            }
-            else if (q==str[x])
-            {
-                qq=0;
-                q=0;            
-            }
-            
-            // sing_q= !sing_q;
-        }
-    // printf("%c",str[x]);
-        // printf("\nq=%c|\n",q);
-       if(str[x]=='$'&& q!='\'' && str[x+1]&& str[x+1]!=' ' && str[x+1]!='$')
-        {
-            if(str[x+1]=='?')
-            {
-                char *ll =ft_itoa(exit_status);
-                i+=ft_strlen(ll);
-                x += 1;
-                free(ll);
-            }
-            else if(str[x+1]&& (str[x+1]==' ' ||  str[x+1]=='\''|| str[x+1]=='"'))
-            {
-                // if((str[x+1]=='"' || str[x+1]=='\''))
-                //     x++;
-            }
-            else   
-            {
-                // printf("v=%s\n",(get_valu_of_var(str+x)));
-                // var = search_env(env,get_valu_of_var(str+x,&x));
-                char *ss= get_valu_of_var(str+x,&x);
-                var = getenv(ss);
-                free(ss);
-                // // printf("dd%s\n",var);
-                i+=ft_strlen(var);
-                ///"| c   qw 23"
-                j =0;
-                if(q!='"')
-                    i++;
-                while (q!='"'&&var && var[j])
-                {
-                    if(j-1>=0&&q!='"' && var[j-1]==' ' &&var[j]!=' ')
-                        i++;
-                    // if(j-1>=0&&q!='"' && var[j-1]!=' ' &&var[j]==' ')
-                    //     re[i++]='"';
-                    
-                    if(q!='"' && var[j]!=' ' &&(var[j+1]==' '|| var[j+1]=='\0'))
-                        i++;
-                    j++;
-                }
-                
-                
-            
-                
-            }
-        
-        
-        }
-        else    
-        {
-            i++;
-            // printf("%c",str[x]);
-        }
-        x++;
-        
-    }
-    // printf("%d\n",i);
-    // if(q!='"')
-    //     i+=2;
-    re =malloc (i+1);
-    x =0;
-    i =0;
-    q=0;
-    qq=0;
-     j =0;
-    while (str[x])
-    {
-        if (str[x] == '\'' ||  str[x] == '"') 
-        {
-            if(qq ==0)
-            {
-                q=str[x];
-                qq++;
-            }
-            else if (q==str[x])
-            {
-                qq=0;
-                q=0;            
-            }
-            
-            // sing_q= !sing_q;
-        }
-    // printf("%c",str[x]);
-        // printf("\nq=%c|\n",q);
-        if(str[x]=='$'&& q!='\'' && str[x+1]&& str[x+1]!=' ' && str[x+1]!='$')
-        {
-            if(str[x+1]=='?')
-            {
-                // i+=ft_strlen(ft_itoa(exit_status));
-                char *sss= ft_itoa(exit_status);
-                int a =0;
-                while (sss[a])
-                {
-                    re[i++]=sss[a++];
-                }
-                free(sss);
+	j = args->x;
+	ss = get_name_var(str + args->x, &(*args).x);
+	var = search_in_env(envp, ss);
+	free(ss);
+	*err = check_erroe_var(var, j, args->l, str);
+	j = 0;
+	if (var && var[j] && args->l != 2)
+		args->re[args->i++] = '"';
+	while (var && var[j])
+	{
+		if (j - 1 >= 0 && args->l != 2 && var[j - 1] == ' ' && var[j] != ' ')
+			args->re[args->i++] = '"';
+		args->re[args->i++] = var[j];
+		if (args->l != 2 && var[j] != ' ' && (var[j + 1] == ' '
+				|| var[j + 1] == '\0'))
+			args->re[args->i++] = '"';
+		j++;
+	}
+}
 
-                
-                x +=1;
-            }
-            else if(str[x+1]&& (str[x+1]==' ' ||  str[x+1]=='\''|| str[x+1]=='"'))//|$ |
-            {
-                // if((str[x+1]=='"' || str[x+1]=='\''))
-                //     x++;
-            }
-            else   
-            {
+char *change_var(char *str, char **envp, int *err)
+{
+	t_args_var	args;
+	t_quote		q;
 
-
-
-
-
-                j =x;
-                // printf("v=%s\n",(get_valu_of_var(str+x)));
-               
-                char *ss= get_valu_of_var(str+x,&x);
-                var = getenv(ss);
-                free(ss);
-
-
-                /////error
-
-                int l =0;
-                int k =0;
-                while (var &&var[l])
-                {
-                    if(var[l]==' ')
-                    {
-                        k =1;
-                        break;
-                    }
-                    l++;
-                }
-                l =j;
-                if (q!='"'&&  str[x])
-                {
-                    while (l>=0)
-                    {
-                        
-                        l--;
-                        //  printf("%c\n",str[l]);   
-                        if((str[l]=='>'||str[l]=='<' ))
-                        {
-                            if((!var || k))
-                                *err = 1;
-                        }
-
-                        if(str[l]!=' ')
-                            break;
-                        
-
-
-                        
-                    }
-                    
-                    
-
-                    
-                    
-                }
-                
-
-
-                //////////
-                // var = search_env(env,get_valu_of_var(str+x,&x));
-                j =0;
-                if( var && var[j] && q!='"')
-                    re[i++]='"';
-                while (var && var[j])
-                {
-                    if(j-1>=0&&q!='"' && var[j-1]==' ' &&var[j]!=' ')
-                        re[i++]='"';
-                    // if(j-1>=0&&q!='"' && var[j-1]!=' ' &&var[j]==' ')
-                    //     re[i++]='"';
-                    re[i++]=var[j];
-                    if(q!='"' && var[j]!=' ' &&(var[j+1]==' '|| var[j+1]=='\0'))
-                        re[i++]='"';
-                    j++;
-                }
-
-                // string   |
-                // string|
-                // if(q!='"')
-                //     re[i++]='"';
-                
-            
-                
-            }
-        
-        
-        }
-        else    
-        {
-            re[i++]= str[x];
-        }
-        x++;
-        
-    }
-    re[i]='\0';
-    return re;
+	q.inDoubleQuote = 0;
+	q.inSingleQuote = 0;
+	args.re = malloc (change_var_count(str, envp, err) + 1);
+	args.x = -1;
+	args.i = 0;
+	while (str[++args.x])
+	{
+		args.l = chacke_q(str[args.x], &q);
+		if (str[args.x] == '$' && args.l != 1 && str[args.x + 1]
+			&& str[args.x + 1] != ' ' && str[args.x + 1] != '$')
+		{
+			if (str[args.x + 1] == '?')
+				change_var_one(&args.x, args.re, &args.i);
+			else if (!(str[args.x + 1] == ' ' || str[args.x + 1] == '\''
+					|| str[args.x + 1] == '"'))
+				change_var_tow(&args, str, err, envp);
+		}
+		else
+			args.re[args.i++] = str[args.x];
+	}
+	args.re[args.i] = '\0';
+	return (args.re);
 }
 
 
-// int main()
-// {
-//     t_envp *env=NULL;
-//     char *str  = "ll $$ $ARG dd";
-//     char *ss= "ll $$ \"echo\" \"|\" \"ls'\" '-l\"    \">\"    \"'\"  \"cat\"   dd";
-//     printf("%s\n",str);
-//     printf("%s\n",change_var(str));
-// }
--- parsing/min.h
+++ parsing/min.h
@@ -6,9 +6,12 @@
 /*   By: hel-bouk <hel-bouk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 18:35:37 by zbakkas           #+#    #+#             */
-/*   Updated: 2024/07/26 13:53:45 by hel-bouk         ###   ########.fr       */
+/*   Updated: 2024/07/30 17:08:38 by hel-bouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef MIN_H
+#define MIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,6 +20,7 @@
 #include "../execution/libft/libft.h"
 #include <stdbool.h>
 
+extern int	exit_status;
 
 # define TOKENS_ERROR ": syntax error near unexpected token"
 # define NEW_LINE_ERROR ": syntax error near unexpected token `newline'"
@@ -29,17 +33,8 @@
 # define AMBIGUOUS_ERROR ": ambiguous redirect"
 // # define SPACE_REDIRECTION_ERROR ": No such file or directory"
 
-#ifndef EXIT_STATUS_DEFINED
-#define EXIT_STATUS_DEFINED
-
-extern int	exit_status;
-
-#endif
 
 /////////////////////////////////////////////////
-
-#ifndef S_ENVP_DEFINED
-#define S_ENVP_DEFINED
 
 typedef struct s_envp
 {
@@ -48,40 +43,46 @@ typedef struct s_envp
 	struct  s_envp	*next;
 }                   t_envp;
 
-#endif // S_ENVP_DEFINED
 /////////////////////////////////////////////
 
 typedef struct  s_quote
 {
-    int inDoubleQuote ;
-    int inSingleQuote ;
+	int inDoubleQuote ;
+	int inSingleQuote ;
 
 }t_quote;
 
 
 typedef struct s_inp
 {
-    char *inp;
-    bool is_h;
+	char *inp;
+	bool is_h;
 
 } t_inp;
 
 typedef struct s_out
 {
-    char *out;
-    bool is_a;
+	char *out;
+	bool is_a;
 
 } t_out;
-
-
+///////change_var////
+typedef struct s_args_var
+{
+	int		x;
+	int		l;
+	char	*re;
+	int		i;
+}	t_args_var;
+/////////////////////
 
 typedef struct s_args_n
 {
-    char **arguments;
-    t_inp *inp;
-    t_out *out;
-    struct s_args_n *next;
-    
+	char **arguments;
+	t_inp *inp;
+	t_out *out;
+	struct s_args_n *next;
+	
 } t_args_n;
 
 
@@ -95,12 +96,17 @@ char *set_speece( char *str) ;
 // char *get_argumants(char *str);
 // char **get_flages(char *str);
 // char *get_cmd(char *str);
-char *change_var(char * str,int *err);
+char *change_var(char * str, char **envp, int *err);
+int	change_var_count(char *str, char **envp, int *err);
+char	*get_name_var(char *str, int *j);
+char	*search_in_env(char **envp, char *str);
+
+
 char **get_args(char **str);
 t_out *get_out(char **str);
 t_inp *get_inp(char **str);
 
-t_args_n *initialization_list(char *line,t_envp *env);
+t_args_n *initialization_list(char *line, char **envp);
 void	ft_lstadd_backk(t_args_n **lst, t_args_n *new);
 t_args_n	*ft_lstnew_one(char **str);
 void	ft_lstiterr(t_args_n *lst);
@@ -110,3 +116,5 @@ char	*whithout_q(char *str);
 
 int check_errors(char *str,int err);
 int chacke_q(char c,t_quote *q);
+
+#endif
